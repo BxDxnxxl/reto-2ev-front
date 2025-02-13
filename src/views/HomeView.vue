ceaeba20a45db0295b2c2767ab564396f11aa207
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-02-13 15:50:30 +0100

    Creacion del carrousel

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
 import Footer from '@/components/Footer.vue'
+import Carrousel from '@/components/Carrousel.vue';
 </script>
 
 
 <template>
   <Header />
+
+  <Carrousel />
 
   <Footer />
 </template>
