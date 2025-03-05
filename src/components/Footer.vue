108b079f78c069b2ba2481b1a56f25e67d487ecc
DESKTOP-S3CMJ72\acent <a26515@svalero.com>
2025-03-05 22:03:43 +0100

    Commit cambios de la HomePage

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -9,26 +9,28 @@ import { RouterLink } from 'vue-router'
       rel="stylesheet"
     />
     <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css" />
-    <div class="footer__column footer__column--socials">
-      <h3 class="footer__title">Redes sociales</h3>
-      <a href="#" class="footer__link">
-        <i class="footer__icon ri-facebook-box-fill"></i>
-      </a>
-      <a href="https://github.com/BxDxnxxl/reto-2ev-front.git" class="footer__link">
-        <i class="footer__icon ri-github-fill"></i>
-      </a>
-      <a href="#" class="footer__link">
-        <i class="footer__icon ri-instagram-fill"></i>
-      </a>
-    </div>
+    <div class="footer__content">
+      <div class="footer__column footer__column--socials">
+        <h3 class="footer__title">Redes sociales</h3>
+        <a href="#" class="footer__link">
+          <i class="footer__icon ri-facebook-box-fill"></i>
+        </a>
+        <a href="https://github.com/BxDxnxxl/reto-2ev-front.git" class="footer__link">
+          <i class="footer__icon ri-github-fill"></i>
+        </a>
+        <a href="#" class="footer__link">
+          <i class="footer__icon ri-instagram-fill"></i>
+        </a>
+      </div>
 
-    <div class="footer__column footer__column--contact">
-      <h3 class="footer__title">Contacto</h3>
-      <p class="footer__info">+34 123456789</p>
-      <p class="footer__info">WannaGames@gmail.com</p>
-      <router-link to="/contact">
-        <a class="footer__button ov-btn-slide-left">Contacto</a>
-      </router-link>
+      <div class="footer__column footer__column--contact">
+        <h3 class="footer__title">Contacto</h3>
+        <p class="footer__info">+34 123456789</p>
+        <p class="footer__info">WannaGames@gmail.com</p>
+        <router-link to="/contact">
+          <a class="footer__button ov-btn-slide-left">Contacto</a>
+        </router-link>
+      </div>
     </div>
   </footer>
 </template>
@@ -42,15 +44,12 @@ import { RouterLink } from 'vue-router'
   --text-color: #ffffff;
   --btn-color: #f25421;
 }
+
 .footer {
-  width: 100%;
-  padding: 20px 0;
+  width: 100vw;
+  padding: 40px 0;
   background: #272727;
   color: var(--text-color);
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 2rem;
   position: relative;
   bottom: 0;
   left: 0;
@@ -58,23 +57,34 @@ import { RouterLink } from 'vue-router'
   box-sizing: border-box;
 }
 
-.footer__column {
+.footer__content {
   width: 100%;
-  max-width: 100%;
+  max-width: 1400px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 0 40px;
+  flex-wrap: wrap;
+}
+
+.footer__column {
   text-align: center;
+  flex: 1;
+  min-width: 250px;
 }
 
 .footer__title {
   font-weight: bold;
-  font-size: 1.25rem;
+  font-size: 1.5rem;
   margin-bottom: 1rem;
   letter-spacing: 1px;
 }
 
 .footer__icon {
   color: var(--text-color);
-  font-size: 1.5rem;
-  margin: 0 0.5rem;
+  font-size: 2rem;
+  margin: 0 1rem;
   transition: color 0.3s;
 }
 
@@ -84,21 +94,21 @@ import { RouterLink } from 'vue-router'
 
 .footer__info {
   margin-bottom: 0.5rem;
-  font-size: 1rem;
+  font-size: 1.2rem;
 }
 
 .footer__button {
   background-color: #333;
   border: none;
   border-radius: 30px;
-  padding: 0.75rem 1.5rem;
+  padding: 1rem 2rem;
   color: var(--text-color);
-  font-size: 1rem;
+  font-size: 1.2rem;
   font-weight: 600;
   cursor: pointer;
-  position: relative;
+  text-decoration: none;
   display: inline-block;
-  text-decoration: none; /* Elimina la decoración del texto */
+  margin-top: 10px;
 }
 
 .footer__button:hover {
@@ -106,67 +116,16 @@ import { RouterLink } from 'vue-router'
   color: #ffffff;
 }
 
-.footer__button::after {
-  content: '';
-  background: var(--btn-color);
-  position: absolute;
-  z-index: -1;
-  padding: 16px 20px;
-  display: block;
-  top: 0;
-  bottom: 0;
-  left: -100%;
-  right: 100%;
-  transition: all 0.35s;
-}
-
-.footer__button:hover::after {
-  left: 0;
-  right: 0;
-  top: 0;
-  bottom: 0;
-  transition: all 0.35s;
-}
-
-@media (min-width: 768px) {
-  .footer {
-    flex-direction: row;
-    justify-content: space-between;
-    padding: 2rem 4rem;
-    text-align: left;
+@media (max-width: 768px) {
+  .footer__content {
+    flex-direction: column;
+    gap: 2rem;
+    padding: 0 20px;
   }
 
   .footer__column {
-    max-width: 300px;
-  }
-
-  .footer__icon {
-    font-size: 2rem;
-    margin: 0 1rem;
-  }
-
-  .footer__title {
-    font-size: 1.5rem;
-    margin-bottom: 1.5rem;
-  }
-
-  .footer__button {
-    font-size: 1.1rem;
-    padding: 1rem 2rem;
-  }
-}
-
-@media (max-width: 768px) {
-  .footer__column--socials {
-    display: flex;
-    justify-content: center;
-    gap: 1rem;
-  }
-
-  .footer__column--contact {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+    width: 100%;
+    text-align: center;
   }
 }
 </style>
