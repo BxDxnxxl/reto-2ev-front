1dea1ab215c994028f019228c97fcb8e142861c6
BxDxnxxl <danielcaton44@gmail.com>
2025-05-12 21:40:21 +0200

    finalizacion de vista

---
+++ src/views/IdeasView.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Ideas from "@/components/Ideas.vue";
+</script>
+
+<template>
+  <div class="ideas-view">
+    <Header />
+    <main class="ideas-view__contenido">
+      <Ideas />
+    </main>
+    <Footer />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.ideas-view {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+
+  &__contenido {
+    flex: 1;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    padding-top: 2rem;
+
+    @media (min-width: 768px) {
+      padding-left: 5%;
+      padding-right: 5%;
+    }
+  }
+}
+</style>
