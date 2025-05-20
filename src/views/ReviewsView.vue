1b2b5f6e7fffd78ad6d8ec916c3a747fc2638624
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-05-20 19:19:46 +0200

    eliminación de componente que no se usa

--- src/views/ReviewsView.vue
+++ src/views/ReviewsView.vue
@@ -2,7 +2,7 @@
 import ReviewPrincipal from '@/components/ReviewPrincipal.vue'
 import GameDetails from '@/components/GameDetails.vue'
 import PersonalExperience from '@/components/PersonalExperience.vue'
-import RatingBox from '@/components/RatingBox.vue'
+
 </script>
 
 <template>
